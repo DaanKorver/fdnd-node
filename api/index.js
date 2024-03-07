53fa73bfd68578813813e814a9a78f58c3d8f2cb
DaanKorver <daank2000@hotmail.nl>
2024-03-07 20:36:19 +0100

    Added EJS

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-	res.send('Express met Vercel. GRATIS!!!')
+	res.render('index', {
+		text: 'Vercel met EJS en Node. For free :)',
+	})
 })
 
 module.exports = app
